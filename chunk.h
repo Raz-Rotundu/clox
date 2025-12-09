96ec796caa300ab8afe946569ccfa2530f5abcde
Razvan Rotundu <raz.rotundu@gmail.com>
2025-12-09 17:23:05 -0500

    Add chunk.c and chunk.h
    chunk.c is an object representing a block of code. Implemented as an array of unsigned bytes.
    Contains two functions:
    	initChunk: Constructor, initializes chunk count and capacity to zero.
    	writeChunk: Adds specified byte to the chunk, resizing array as required

---
+++ chunk.h
@@ -0,0 +1,27 @@
+#fndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+// A module which defines code representation
+// A chunk is  a sequence of bytecode
+
+// Represents the opCodes of the virtual machine
+typedef enum {
+  OP_RETURN,
+  
+} OpCode;
+
+// A dynamic array containing the bytes of the code(chunk)
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+} Chunk;
+
+// Initialize a new chunk
+void initChunk(Chunk* chunk);
+void writeCHunk(Chunk* chunk, uint8_t byte);
+
+
+#endif
---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef clox_common_h
+#define clox_common_h
+
+// This file is just a container for libs we'll need throughout the interpreter
+/**
+Types and constants needed
+    NULL
+    size_t
+    bool
+    uint8_t
+ */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
